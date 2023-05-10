7701a27f37a2419113efa48cb07a8904115b068d
Jung In Kim <vindictuslbp@gmail.com>
2023-05-10 12:16:26 -0400

    Added validate function to catch any user entering empty inputs or invalid inputs.

--- index.js
+++ index.js
@@ -8,15 +8,33 @@ inquirer
             type: 'input',
             name: 'text',
             message: 'Enter up to three characters:',
-            validate: (value) => {
-                const valid = value.length <= 3;
-                return valid || 'Please enter up to three characters.';
+            validate: (input) => {
+                const validLength = input.length <= 3;
+                const validInput = input.trim() !== '';
+                if (!validLength) {
+                  return 'Please enter up to three characters only.';
+                }
+                if (!validInput) {
+                  return 'Empty inputs are not allowed, please try again.';
+                }
+                return true;
             },
         },
         {
             type: 'input',
             name: 'textColor',
             message: 'Enter the text color (color keyword or hexadecimal number):',
+            validate: (input) => {
+                const validLength = input.length > 0;
+                const validColor = /^#?([a-fA-F0-9]{3}|[a-fA-F0-9]{6})$/.test(input);
+                if (!validLength) {
+                  return 'Empty inputs are not allowed, please try again.';
+                }
+                if (!validColor) {
+                  return 'Please enter a valid color keyword or hexadecimal number.';
+                }
+                return true;
+            },
         },
         {
             type: 'list',
@@ -28,6 +46,17 @@ inquirer
             type: 'input',
             name: 'shapeColor',
             message: 'Enter the shape color (color keyword or hexadecimal number):',
+            validate: (input) => {
+                const validLength = input.length > 0;
+                const validColor = /^#?([a-fA-F0-9]{3}|[a-fA-F0-9]{6})$/.test(input);
+                if (!validLength) {
+                  return 'Empty inputs are not allowed, please try again.';
+                }
+                if (!validColor) {
+                  return 'Please enter a valid color keyword or hexadecimal number.';
+                }
+                return true;
+            },
         },
     ])
     .catch((error) => {
