02b92f32ec83e6119d579c39716045bc1028c32f
Jung In Kim <vindictuslbp@gmail.com>
2023-05-11 21:04:43 -0400

    Added comments in index.js and shapes.js. Fixed a mistake with the .svg file name to match the acceptence criteria properly.

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
+// Global variables for requirement to use Node modules.
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+// Global variable to use local module to define the shape classes.
 const { Circle, Triangle, Square } = require('./lib/shapes');
 
+// Inquirer promise chain to prompt user with questions.
 inquirer
     .prompt([
         {
+            // Prompt for text input up to 3 character limit.
             type: 'input',
             name: 'text',
             message: 'Enter up to three characters:',
+            // Validate function to prevent more than 3 characters & empty input string.
             validate: (input) => {
                 const validLength = input.length <= 3;
                 const validInput = input.trim() !== '';
@@ -21,9 +27,11 @@ inquirer
             },
         },
         {
+            // Prompt for text input for text colour.
             type: 'input',
             name: 'textColour',
-            message: 'Enter the text colour (colour keyword or [#]hexadecimal number):',
+            message: 'Enter the text colour ([i.e. blue, red, black] colour keyword or [#]hexadecimal number):',
+            // Validate function to prevent empty inputs & check if the input is valid.
             validate: (input) => {
                 const validLength = input.length > 0;
                 const validColor = /^#?([a-fA-F0-9]{3}|[a-fA-F0-9]{6})$|^([a-z]+)$/i.test(input);
@@ -37,15 +45,18 @@ inquirer
             },
         },
         {
+            // Prompt to give a list of shapes to choose from for the user.
             type: 'list',
             name: 'shape',
             message: 'Choose a shape:',
             choices: ['circle', 'triangle', 'square'],
         },
         {
+            // Prompt for text input for shape colour.
             type: 'input',
             name: 'shapeColour',
-            message: 'Enter the shape colour (colour keyword or [#]hexadecimal number):',
+            message: 'Enter the shape colour ([i.e. blue, red, black] colour keyword or [#]hexadecimal number):',
+            // Validate function to prevent empty inputs & check if the input is valid.
             validate: (input) => {
                 const validLength = input.length > 0;
                 const validColor = /^#?([a-fA-F0-9]{3}|[a-fA-F0-9]{6})$|^([a-z]+)$/i.test(input);
@@ -59,8 +70,10 @@ inquirer
             },
         },
     ])
+    // Then function to transfer user answers into correct shapes to be generated.
     .then((answers) => {
         let shape;
+        // Checks the value of user's shape answer and assign to a shape object.
         switch (answers.shape) {
             case 'circle':
                 shape = new Circle(answers.shapeColour);
@@ -74,10 +87,13 @@ inquirer
             default:
                 throw new Error(`Invalid shape: ${answers.shape}`);
         }
+        // Create an SVG string to pass in user's shape object for text and text colour.
         const svg = shape.toSVG(answers.text, answers.textColour);
-        fs.writeFileSync(`examples/${answers.shape}.svg`, svg);
-        console.log(`Successfully generated ${answers.shape}.svg in the examples folder!`);
+        // Writes the string into a .svg file under examples folder.
+        fs.writeFileSync(`examples/logo.svg`, svg);
+        console.log(`Successfully generated logo.svg in the examples folder!`);
     })
+    // Catches any errors in the promise chain.
     .catch((error) => {
         console.error(error);
     });
